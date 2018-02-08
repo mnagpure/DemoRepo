20eb68145ea7344524517487e95540cbabb6706d
mnagpure <manya879@gmail.com>
2018-02-08 16:09:39 +0530

    push 1st time

---
+++ src/main/java/com/qa/pages/HomePage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class HomePage {
+
+	public void HomePage1() {
+		System.out.println("Home Page");
+	}
+}
---
+++ src/main/java/com/qa/util/TestUtil.java
@@ -0,0 +1,8 @@
+package com.qa.util;
+
+public class TestUtil {
+
+	public void TestUtil1() {
+		System.out.println("TestUtil Page");
+	}
+}
